b4354fe8efc2dc04f34ca96d3ffe8db996b90df2
Laís Urano <lais.urano@gmail.com>
2024-05-24 12:04:25 -0300

    adicionando throttles

---
+++ escola/views.py
@@ -0,0 +1,38 @@
+from escola.models import Estudante,Curso, Matricula
+from escola.serializers import EstudanteSerializer,CursoSerializer, MatriculaSerializer, ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer, EstudanteSerializerV2
+from rest_framework import viewsets, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.throttling import UserRateThrottle
+from escola.throttles import MatriculaAnonRateThrottle
+
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all().order_by("id")
+    #serializer_class = EstudanteSerializer
+    filter_backends = [DjangoFilterBackend,filters.OrderingFilter,filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome','cpf']
+    def get_serializer_class(self):
+        if self.request.version == 'v2':
+            return EstudanteSerializerV2
+        return EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all().order_by("id")
+    serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all().order_by("id")
+    serializer_class = MatriculaSerializer
+    throttle_classes = [UserRateThrottle,MatriculaAnonRateThrottle]
+
+class ListaMatriculaEstudante(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']).order_by("id")
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializer
+
+class ListaMatriculaCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk']).order_by("id")
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
